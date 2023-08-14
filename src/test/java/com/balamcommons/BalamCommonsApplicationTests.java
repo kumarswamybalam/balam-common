3844e41cbb40892e0cbf96d5aa36080843eed56d
Kumar Swamy Balam <kumarswamybalam@Kumars-MacBook-Air.local>
2023-08-14 16:57:48 -0500

    inital commit

---
+++ src/test/java/com/balamcommons/BalamCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balamcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalamCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
